46c65c4f562c1f9476ed5a8517e6c37ed4cec522
Brian Duff <brian@dufflin.com>
2023-11-26 22:46:48 -0800

    Fix css to use dark mode and include blueprintjs stylesheets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-import { Button } from '@blueprintjs/core'
+import { Alignment, Button, Navbar } from '@blueprintjs/core'
 import './App.css'
 import { css } from '@emotion/react'
 
 function App() {
   return (
     <>
+      <Navbar fixedToTop={true} >
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Hello</Navbar.Heading>
+          <Navbar.Divider />
+        </Navbar.Group>
+      </Navbar>
       <Button>Let's go</Button>
       <div css={css`color: red`}>Hello</div>
     </>
